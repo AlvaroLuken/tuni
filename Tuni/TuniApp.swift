1b09fb525ecf7d89bc7bd54395e1fe824f24c226
AlvaroLuken <al5031@nyu.edu>
2025-05-30 17:41:47 -0700

    Initial Commit

---
+++ Tuni/TuniApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuniApp.swift
+//  Tuni
+//
+//  Created by Alvaro Luken on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct TuniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TuniTests/TuniTests.swift
@@ -0,0 +1,17 @@
+//
+//  TuniTests.swift
+//  TuniTests
+//
+//  Created by Alvaro Luken on 5/30/25.
+//
+
+import Testing
+@testable import Tuni
+
+struct TuniTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
